b9f15298fd9c23df1b5b87fb008aa0a0580f10bb
nanna3210 <navkishoredas@gmail.com>
2022-01-23 23:59:24 +0530

    table name Changed

--- src/main/java/com/nanna/entity/Employee.java
+++ src/main/java/com/nanna/entity/Employee.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -20,6 +21,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "employeeType")
 public abstract class Employee {
 
 	@Id
